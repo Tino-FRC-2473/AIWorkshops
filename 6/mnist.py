16805d73b88b27ae5cdefbd4732573e3b27cad82
mvoodarla <mvoodarla@gmail.com>
2018-04-13 22:13:56 -0700

    hack cupertino commit

--- 6/mnist.py
+++ 6/mnist.py
@@ -8,7 +8,7 @@ from keras import backend as K
 
 batch_size = 128
 num_classes = 10
-epochs = 1
+epochs = 12
 img_rows, img_cols = 28, 28
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -29,7 +29,12 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-"""YOUR MODEL GOES HERE"""
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
@@ -39,12 +44,13 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
+
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
-model.save("model.h5")
+model.save("model12.h5")
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
